4fbe43618071963e11319078fff616f6e10d63b4
jzsyuan <jzsyuan@gmail.com>
2020-11-13 16:57:23 +0800

    update Program.php

--- Registration.php
+++ Registration.php
@@ -67,130 +67,63 @@
 		</div>
   </section>
   
+        <!-- START THE FEATURETTES -->
+
+
 <section class="ftco-section">
   <div class="container">
     <div class="row">
       <div class="col-lg-8 ftco-animate">
-        <h2 class="mb-3">It is a long established fact a reader be distracted</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis, eius mollitia suscipit, quisquam doloremque distinctio perferendis et doloribus unde architecto optio laboriosam porro adipisci sapiente officiis nemo accusamus ad praesentium? Esse minima nisi et. Dolore perferendis, enim praesentium omnis, iste doloremque quia officia optio deserunt molestiae voluptates soluta architecto tempora.</p>
-        <p>
-          <img src="images/image_3.jpg" alt="" class="img-fluid">
-        </p>
-        <p>Molestiae cupiditate inventore animi, maxime sapiente optio, illo est nemo veritatis repellat sunt doloribus nesciunt! Minima laborum magni reiciendis qui voluptate quisquam voluptatem soluta illo eum ullam incidunt rem assumenda eveniet eaque sequi deleniti tenetur dolore amet fugit perspiciatis ipsa, odit. Nesciunt dolor minima esse vero ut ea, repudiandae suscipit!</p>
-        <h2 class="mb-3 mt-5">#2. Creative WordPress Themes</h2>
-        <p>Temporibus ad error suscipit exercitationem hic molestiae totam obcaecati rerum, eius aut, in. Exercitationem atque quidem tempora maiores ex architecto voluptatum aut officia doloremque. Error dolore voluptas, omnis molestias odio dignissimos culpa ex earum nisi consequatur quos odit quasi repellat qui officiis reiciendis incidunt hic non? Debitis commodi aut, adipisci.</p>
-        <p>
-          <img src="images/image_4.jpg" alt="" class="img-fluid">
-        </p>
-        <p>Quisquam esse aliquam fuga distinctio, quidem delectus veritatis reiciendis. Nihil explicabo quod, est eos ipsum. Unde aut non tenetur tempore, nisi culpa voluptate maiores officiis quis vel ab consectetur suscipit veritatis nulla quos quia aspernatur perferendis, libero sint. Error, velit, porro. Deserunt minus, quibusdam iste enim veniam, modi rem maiores.</p>
-        <p>Odit voluptatibus, eveniet vel nihil cum ullam dolores laborum, quo velit commodi rerum eum quidem pariatur! Quia fuga iste tenetur, ipsa vel nisi in dolorum consequatur, veritatis porro explicabo soluta commodi libero voluptatem similique id quidem? Blanditiis voluptates aperiam non magni. Reprehenderit nobis odit inventore, quia laboriosam harum excepturi ea.</p>
-        <p>Adipisci vero culpa, eius nobis soluta. Dolore, maxime ullam ipsam quidem, dolor distinctio similique asperiores voluptas enim, exercitationem ratione aut adipisci modi quod quibusdam iusto, voluptates beatae iure nemo itaque laborum. Consequuntur et pariatur totam fuga eligendi vero dolorum provident. Voluptatibus, veritatis. Beatae numquam nam ab voluptatibus culpa, tenetur recusandae!</p>
-        <p>Voluptas dolores dignissimos dolorum temporibus, autem aliquam ducimus at officia adipisci quasi nemo a perspiciatis provident magni laboriosam repudiandae iure iusto commodi debitis est blanditiis alias laborum sint dolore. Dolores, iure, reprehenderit. Error provident, pariatur cupiditate soluta doloremque aut ratione. Harum voluptates mollitia illo minus praesentium, rerum ipsa debitis, inventore?</p>
-        <div class="tag-widget post-tag-container mb-5 mt-5">
-          <div class="tagcloud">
-            <a href="#" class="tag-cloud-link">Life</a>
-            <a href="#" class="tag-cloud-link">Sport</a>
-            <a href="#" class="tag-cloud-link">Tech</a>
-            <a href="#" class="tag-cloud-link">Travel</a>
-          </div>
-        </div>
-        
-        <div class="about-author d-flex p-4 bg-light">
-          <div class="bio mr-5">
-            <img src="images/person_1.jpg" alt="Image placeholder" class="img-fluid mb-4">
-          </div>
-          <div class="desc">
-            <h3>George Washington</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessitatibus voluptate quod mollitia delectus aut, sunt placeat nam vero culpa sapiente consectetur similique, inventore eos fugit cupiditate numquam!</p>
-          </div>
-        </div>
-
-
-        <div class="pt-5 mt-5">
-          <h3 class="mb-5">6 Comments</h3>
-          <ul class="comment-list">
-            <li class="comment">
-              <div class="vcard bio">
-                <img src="images/person_1.jpg" alt="Image placeholder">
-              </div>
-              <div class="comment-body">
-                <h3>John Doe</h3>
-                <div class="meta">August 12, 2020 at 2:21pm</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                <p><a href="#" class="reply">Reply</a></p>
-              </div>
-            </li>
-
-            <li class="comment">
-              <div class="vcard bio">
-                <img src="images/person_1.jpg" alt="Image placeholder">
-              </div>
-              <div class="comment-body">
-                <h3>John Doe</h3>
-                <div class="meta">August 12, 2020 at 2:21pm</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                <p><a href="#" class="reply">Reply</a></p>
-              </div>
-
-              <ul class="children">
-                <li class="comment">
-                  <div class="vcard bio">
-                    <img src="images/person_1.jpg" alt="Image placeholder">
-                  </div>
-                  <div class="comment-body">
-                    <h3>John Doe</h3>
-                    <div class="meta">August 12, 2020 at 2:21pm</div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                    <p><a href="#" class="reply">Reply</a></p>
-                  </div>
-
-                </li>
-              </ul>
-            </li>
-
-            <li class="comment">
-              <div class="vcard bio">
-                <img src="images/person_1.jpg" alt="Image placeholder">
-              </div>
-              <div class="comment-body">
-                <h3>John Doe</h3>
-                <div class="meta">August 12, 2020 at 2:21pm</div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur quidem laborum necessitatibus, ipsam impedit vitae autem, eum officia, fugiat saepe enim sapiente iste iure! Quam voluptas earum impedit necessitatibus, nihil?</p>
-                <p><a href="#" class="reply">Reply</a></p>
-              </div>
-            </li>
+        <h2 class="mb-3">報名費用</h2>
+        <table class="table table-bordered">
+          <thead>
+            <tr>
+              <td class="td_sign1" colspan="2">身分別<br>Category</td>
+              <td >早鳥線上註冊費(7/15前)<br>	Online registration</td>
+              <td >線上註冊費(7/16-7/31)<br>Online registration</td>
+              <td >現場註冊費(8/20-8/21)<br>On-site registration</td>
+            </tr>
+            <tr>
+              <td class="td_sign1" colspan="2">學生<br>Student</td>
+              <td >500</td>
+              <td  >800</td>
+              <td >1000</td>
+            </tr>
+            <tr>
+              <td class="td_sign1" width="83" rowspan="2">非學生<br>Non-Student</td>
+              <td  width="123">CIPS會員<br>CIPS member</td>
+              <td >1000</td>
+              <td >1600</td>
+              <td>2000</td>
+            </tr>
+            <tr>
+              <td  width="123">非CIPS會員<br>Non-CIPS member</td>
+              <td >1200</td>
+              <td >2000</td>
+              <td>2500</td>
+            </tr>
+            <tr>
+              <td class="td_sign1" colspan="2">陪同眷屬**<br>Accompanying person**</td>
+              <td >600</td>
+              <td>800</td>
+               <td>1000</td>
+            </tr>
+            </tbody>
+      </table>
+    <div class="col-lg-8 ftco-animate">
+		  <h2 class="mb-3">線上報名</h2>
+          <ul>
+            <li>線上報名截止日期: <p style="color:red;">2020/7/15。</p></li>
+            <li>研討會期間也接受現場報名。</li>
           </ul>
-          <!-- END comment-list -->
-          
-          <div class="comment-form-wrap pt-5">
-            <h3 class="mb-5">Leave a comment</h3>
-            <form action="#" class="p-5 bg-light">
-              <div class="form-group">
-                <label for="name">Name *</label>
-                <input type="text" class="form-control" id="name">
-              </div>
-              <div class="form-group">
-                <label for="email">Email *</label>
-                <input type="email" class="form-control" id="email">
-              </div>
-              <div class="form-group">
-                <label for="website">Website</label>
-                <input type="url" class="form-control" id="website">
-              </div>
+          <p>
+          <img src="images/NCU3.jpg" alt="" class="img-fluid">
+        </p>
+	  </div> <!-- .col-md-8 -->
 
-              <div class="form-group">
-                <label for="message">Message</label>
-                <textarea name="" id="message" cols="30" rows="10" class="form-control"></textarea>
-              </div>
-              <div class="form-group">
-                <input type="submit" value="Post Comment" class="btn py-3 px-4 btn-primary">
-              </div>
 
-            </form>
-          </div>
-        </div>
 
-      </div> <!-- .col-md-8 -->
+</div> <!-- .col-md-8 -->
       <div class="col-lg-4 sidebar ftco-animate">
         <div class="sidebar-box">
           <form action="#" class="search-form">
