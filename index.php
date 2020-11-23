2bda6c5c8550245820b9c8134f4f9eab2b305c69
jzsyuan <jzsyuan@gmail.com>
2020-11-23 11:31:52 +0800

    index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
 
 	<link rel="stylesheet" href="css/animate.css">
-	
+	<link rel='stylesheet' href='https://unpkg.com/vanilla-datatables@latest/dist/vanilla-dataTables.min.css'>
 	<link rel="stylesheet" href="css/owl.carousel.min.css">
 	<link rel="stylesheet" href="css/owl.theme.default.min.css">
 	<link rel="stylesheet" href="css/magnific-popup.css">
@@ -69,18 +69,40 @@
 	<section class="ftco-section ftco-counter ftco-no-pt ftco-no-pb" id="about-section">
 		<div class="container">
 			<div class="row">
-				<div class="col-md-6 d-flex align-items-stretch">
+				<div class="col-md-5 d-flex align-items-stretch">
 					<div class="about-wrap img w-100" style="background-image: url(images/about1.jpg);">
 					</div>
 				</div>
-				<div class="col-md-6 py-5 pl-md-5">
+				<div class="col-md-7 py-5 pl-md-5">
 					<div class="row justify-content-center mb-4 pt-md-4">
 						<div class="col-md-12 heading-section ftco-animate">
-							<span class="subheading">(主辦人)中央統研所</span>
-							<h2 class="mb-4">歡迎參加中央統計研討會 </h2>
-							<p> 為了促進統計界</p>
+							<h2 class="mb-4">最新資訊 </h2>
+
 						</div>
 					</div>
+							<table class="table">
+	                			<thead>
+		                		<tr>
+			                		<th data-type="date" data-format="YYYY/MM/DD">Start Date</th>
+			                		<th>title</th>
+			                		<th>content</th>
+		                		</tr>
+	                			</thead>
+	            				<tbody>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td><span data-toggle="modal" data-target="#info01" class="text-primary speaker-info">報名費流程已...more </span></td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		           					<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		           			 		<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+		            				<tr><td>2005/02/11</td><td>報名事宜</td><td>123456</td></tr>
+	            				</tbody>
+							</table>
+						
+	
 					<div class="row">
 						<div class="col-md-6 d-flex justify-content-center counter-wrap ftco-animate">
 							<div class="block-18">
@@ -116,93 +138,42 @@
 						</div>
 					</div>
 				</div>
+
+
+
+
+	<div class="modal fade" id="info01" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header" style="background-color:teal;">
+                        <div class="row">
+                            <div class="col-md-3"><img src="images/person_1.jpg" class="rounded-sm"  width="125%"></div>
+                            <div class="col-md-9">
+                                <h3 class="modal-title" id="myModalLabel">中央統研所 Hung-yi Lee</h3>
+                                    <h5>報名流程</h5>
+										<a class="btn btn-primary" href="http://speech.ee.ntu.edu.tw/~tlkagk/index.html" role="button">KKTIX</a>
+                            </div>
+
+                        </div>
+                </div>
+            <div class="modal-body">
+                <p>
+				人之初，性本善，性相近，習相遠，苟不教，性乃遷，教之道，貴以專，昔孟母，擇鄰處，子不學，斷機杼，竇燕山，
+				有義方，教五子，名俱揚，養不教，父之過，教不嚴，師之惰，子不學，非所宜，幼不學，老何為，玉不琢，不成器，
+				人不學，不知義，為人子，方少時，親師友，習禮儀，香九齡，能溫席，孝於親，所當執，融四歲，能讓梨，弟於長，
+				宜先知，首孝弟，次見聞，知某數，識某文，一而十，十而百，百而千，千而萬，三才者，天地人，三光者，日月星，
+				三綱者，君臣義，父子親，夫婦順，日春夏，日秋冬，此四時，運不窮，曰南北，曰西東，此四方，應乎中，
+				</p>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+            </div>
 			</div>
-		</div>
+        </div>
+	</div>
+
 	</section>
 
-
-
-	<?php include_once "news.php"?>
-	<section class="ftco-section ftco-no-pt ftco-no-pb ftco-services-2 bg-primary">
-		<div class="container">
-			<div class="row d-flex">
-				<div class="col-md-7 py-5">
-					<div class="py-lg-5">
-						<div class="row justify-content-center pb-5">
-							<div class="col-md-12 heading-section ftco-animate fadeInUp ftco-animated">
-								<h2 class="mb-3">需求訪談</h2>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate fadeInUp ftco-animated">
-								<div class="media block-6 services d-flex">
-									<div class="icon justify-content-center align-items-center d-flex"></div>
-									<div class="media-body pl-4">
-										<h3 class="heading mb-3"></h3>
-										<p></p>
-									</div>
-								</div>      
-							</div>
-							<div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate fadeInUp ftco-animated">
-								<div class="media block-6 services d-flex">
-									<div class="icon justify-content-center align-items-center d-flex"></div>
-									<div class="media-body pl-4">
-										<h3 class="heading mb-3"></h3>
-										<p></p>
-									</div>
-								</div>      
-							</div>
-							<div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate fadeInUp ftco-animated">
-								<div class="media block-6 services d-flex">
-									<div class="icon justify-content-center align-items-center d-flex"></div>
-									<div class="media-body pl-4">
-										<h3 class="heading mb-3"></h3>
-										<p></p>
-									</div>
-								</div>      
-							</div>
-							<div class="col-md-12 col-lg-6 d-flex align-self-stretch ftco-animate fadeInUp ftco-animated">
-								<div class="media block-6 services d-flex">
-									<div class="icon justify-content-center align-items-center d-flex"></div>
-									<div class="media-body pl-4">
-										<h3 class="heading mb-3"></h3>
-										<p></p>
-									</div>
-								</div>      
-							</div>
-						</div>
-					</div>
-				</div>
-				<div class="col-md-5 d-flex">
-					<div class="appointment-wrap p-4 p-lg-5 d-flex align-items-center">
-						<div class="overlay"></div>
-						<form action="demand.php" class="appointment-form ftco-animate fadeInUp ftco-animated">
-							<h3>需求訪談</h3>
-							<div class="">
-								<div class="form-group">
-									<input type="text" class="form-control" placeholder="Name">
-								</div>
-								<div class="form-group">
-									<input type="text" class="form-control" placeholder="mail">
-								</div>
-								<div class="form-group">
-									<input type="text" class="form-control" placeholder="Phone">
-								</div>
-							</div>
-							<div class="">
-								<div class="form-group">
-									<textarea name="" id="" cols="30" rows="4" class="form-control" placeholder="Message"></textarea>
-								</div>
-								<div class="form-group">
-									<input type="submit" value="Appointment" class="btn btn-primary py-3 px-4">
-								</div>
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
 
 	<section class="ftco-intro img" style="background-image: url(images/bg_4.jpg);">
 		<div class="overlay" ></div>
@@ -219,5 +190,17 @@
 
 	<?php include_once "sponsor.php" ?>
 	<?php include_once "footer.php" ?>
+<script src="https://static.codepen.io/assets/common/stopExecutionOnTimeout-157cd5b220a5c80d4ff8e0e70ac069bffd87a61252088146915e8726e5d9f147.js"></script>
+<script src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/86186/moment.js'></script>
+<script src='https://unpkg.com/vanilla-datatables@latest/dist/vanilla-dataTables.min.js'></script>
+<script id="rendered-js" > var table = new DataTable("table"); </script>
+<script>
+    if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+    }
+</script>
+<script>
+    window.console = window.console || function(t) {};
+  </script>
 	</body>
 	</html>
