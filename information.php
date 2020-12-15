b261c51627d9c54f9a8f705d4ce4f42ef8d5d9a0
yuroylee <yuyu1798@gmail.com>
2020-12-15 22:34:44 +0800

    123

---
+++ information.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="zh-tw">
+<head>
+<title>講者資訊</title>	<meta charset="utf-8">
+	<link rel="icon" href="images/NCU.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700;900&display=swap" rel="stylesheet">
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="css/animate.css">
+	
+	<link rel="stylesheet" href="css/owl.carousel.min.css">
+	<link rel="stylesheet" href="css/owl.theme.default.min.css">
+	<link rel="stylesheet" href="css/magnific-popup.css">
+
+	<link rel="stylesheet" href="css/bootstrap-datepicker.css">
+	<link rel="stylesheet" href="css/jquery.timepicker.css">
+	
+	<link rel="stylesheet" href="css/flaticon.css">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+<header>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-dark ftco-navbar-light" id="ftco-navbar">
+		<div class="container d-flex align-items-center">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="fa fa-bars"></span> Menu
+			</button>
+			<div class="collapse navbar-collapse" id="ftco-nav">
+          	<ul class="navbar-nav mr-auto">
+            	<li class="nav-item ">
+              	<a class="nav-link" href="index.php">大會資訊/Conference <span class="sr-only">(current)</span></a>
+            	</li>
+           	 	<li class="nav-item">
+              	<a class="nav-link" href="Program.php">研討會議程 / Program</a>
+            	</li>
+            	<li class="nav-item active">
+              	<a class="nav-link" href="information.php">講者資訊</a>
+            	</li>
+            	<li class="nav-item">
+             	 <a class="nav-link" href="Registration.php">報名 / Registration</a>
+            	</li>
+            	<li class="nav-item">
+              	<a class="nav-link" href="Other.php">其他資訊 / Others</a>
+            	</li>
+          	</ul>
+        	</div>
+		</div>
+	</nav>
+</header>
+	<!-- END nav -->
+	<section class="hero-wrap js-fullheight" style="background-image: url('images/BG5.jpg');" data-stellar-background-ratio="0.5">
+		<div class="overlay"></div>
+		<div class="container">
+			<div class="row no-gutters slider-text js-fullheight align-items-center justify-content-center" data-scrollax-parent="true">
+				<div class="col-lg-9 ftco-animate text-center">
+					<div class="mt-5">
+					<h1 class="mb-4"><font color="#FAFAFA">110年第一屆統計傳薪研討會</font></h1>
+					    <h2><font color="#FAFAFA">會議簡介</font></h2>
+					        <p><font color="#FAFAFA">第一屆統計傳薪學術研討會由國立中央大學統計研究所主辦，會議在國立中央大學舉行，並與科技部數學研究推動中心、中央研究院統計科學研究所、中華機率統計學會、國立中央大學理學院、及台灣量化交易協會合辦。</br>
+                            研討會源自於中華機率統計學會理事長黃文瀚教授的提議，希望提供年輕統計學者們一個互動交流的舞台，並由有經驗或資深的師長們傳承相關經歷。接受薪傳的年輕統計學者們為後繼者與接棒者，皆已象徵統計新苗與新秀之意，故國立中央大學統計研究所將會議名稱訂為「統計傳薪學術研討會」。國立中央大學統計研究所為國內最早之獨立統計研究所，因此主辦此第一屆研討會別具意義。</br>
+                            大會主講人將以經驗傳承為主軸，分享教學、研究、與服務的心路歷程，各場次邀請的主角則為新進教師與博士後研究員，同時亦邀請各學術單位的主管、師長、與同學們參與，期望藉由會議的過程給予年輕統計學者們各方面的協助與諮詢，使統計傳薪學術研討會發揮最大的功效。</font>
+                            </p>
+						<p><a href="#" class="btn btn-primary">報名方式</a>
+							<a href="#" class="btn btn-white">流程</a></p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+<section class="ftco-section">
+  <div class="container">
+                    <div class="row">
+                        <div class="col-md-12  ftco-animate">
+	                        <div class="about-author d-flex p-4 bg-light">
+                                <div class="bio mr-5">
+                                    <img src="images/person1.jpg" alt="Image placeholder" class="img-fluid mb-4">
+                                </div>
+                                    <div class="desc">
+                                        <h3>大會獎者-銀慶剛</h3>
+                                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus itaque, autem necessita.....</p>
+                                    </div>
+                                </div>
+                        </div>
+                    </div>
+  </div>
+</section>
+
+<!-- Portfolio Grid Section -->
+    <section id="schedule">
+      <div class="container  invit-table">
+          <div class="section-header wow fadeInUp">
+            <h3 class="text-center title">議程場次</h3>
+			      <div class="separator"></div>
+          </div>
+          <div class="row">
+            <!-- 這裡開始 -->
+
+<div class="col-12 col-sm-12 col-md-12">
+<div class="table-responsive">
+      <table class="table table-hover">
+        <tbody>
+          <tr>
+            <th width="4%" text-align="center" valign="middle" bgcolor="#E8DBD0" scope="row">No</th>
+            <th width="60%" align="center" valign="middle" bgcolor="#F7ECD6">Session Title</th>
+            <th width="6%" align="center" valign="middle" bgcolor="#F7ECD6">Organizer(s)</th>
+            <th width="6%" align="center" valign="middle" bgcolor="#F7ECD6">Chair</th>
+            <th width="6%" align="center" valign="middle" bgcolor="#F7ECD6">Invited Speaker(1)</th>
+            <th width="6%" align="center" valign="middle" bgcolor="#F7ECD6">Invited Speaker(2)</th>
+            <th width="6%" align="center" valign="middle" bgcolor="#F7ECD6">Invited Speaker(3)</th>
+            <th width="6%" align="center" valign="middle" bgcolor="#F7ECD6">Invited Speaker(4)</th>
+            </tr>
+            <tr>
+            <td valign="top" bgcolor="#FFFFFF">1</td>
+            <td align="center" bgcolor="#FFFFFF">長期資料分析</td>
+            <td align="center" bgcolor="#FFFFFF"><a href="http://www.stat.fcu.edu.tw/wSite/ct?xItem=a8f73866141ed95c&ctNode=11040&mp=440101&submit=%E5%80%8B%E4%BA%BA%E8%B3%87%E6%96%99" target="_blank">王婉倫</a></td>
+            <td align="center" bgcolor="#FFFFFF"><a href="http://www.stat.fcu.edu.tw/wSite/ct?xItem=a8f73866141ed95c&ctNode=11040&mp=440101&submit=%E5%80%8B%E4%BA%BA%E8%B3%87%E6%96%99" target="_blank">王婉倫</a></td>
+            <td align="center" bgcolor="#FFFFFF"><a href="https://pure.ncue.edu.tw/zh/persons/miao-yu-tsai" target="_blank">蔡秒玉</a></td>
+            <td align="center" bgcolor="#FFFFFF"><a href="http://www.math.ccu.edu.tw/math/?page_id=232" target="_blank">沈仲維</a></td>
+            <td align="center" bgcolor="#FFFFFF"><a href="http://www.stat.fcu.edu.tw/wSite/ct?xItem=a8f73866141ed95c&ctNode=11040&mp=440101&submit=%E5%80%8B%E4%BA%BA%E8%B3%87%E6%96%99" target="_blank">王婉倫</a></td>
+            <td align="center" bgcolor="#FFFFFF"></td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#FFFFFF">2</td>
+              <td align="center" bgcolor="#FFFFFF">Machine Learning and Data Mining</td>
+              <td align="center" bgcolor="#FFFFFF"><a href="http://www.stat.tku.edu.tw/info/11" target="_blank">李百靈</a></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.tku.edu.tw/info/11" target="_blank">李百靈</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.math.ccu.edu.tw/math/?page_id=194" target="_blank">史玉山</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://me.ntue.edu.tw/about/staffDetail/60" target="_blank">林松江</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.tku.edu.tw/info/107" target="_blank">高君豪</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"></td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#FFFFFF">3</td>
+              <td align="center" bgcolor="#FFFFFF">Advanced Machine Learning</td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://www.math.nctu.edu.tw/faculty/faculty_content.php?S_ID=124&SC_ID=1" target="_blank">李育杰</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://www.math.nctu.edu.tw/faculty/faculty_content.php?S_ID=124&SC_ID=1" target="_blank">李育杰</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://www.ee.ntu.edu.tw/profile1.php?teacher_id=901182&p=3" target="_blank">李宏毅</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.sinica.edu.tw/personinfo/?sysid=6002497" target="_blank">李靜沛</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://walonchiu.github.io/" target="_blank">邱維辰</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"></td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#FFFFFF">4</td>
+              <td align="center" bgcolor="#FFFFFF">Financial Time Series<br>
+(財務時間序列)</td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.math.nsysu.edu.tw/~guomh/index.html" target="_blank">郭美惠</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.math.nsysu.edu.tw/~guomh/index.html" target="_blank">郭美惠</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://math.nuk.edu.tw/p/405-1018-586,c95.php?Lang=zh-tw" target="_blank">黃士峰</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncu.edu.tw/index.php/faculty/more/162" target="_blank">孫立憲</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncku.edu.tw/index.php?option=module&lang=cht&task=pageinfo&id=259&index=13" target="_blank">林良靖</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"></td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#FFFFFF">5</td>
+              <td align="center" bgcolor="#FFFFFF">空間統計</td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncu.edu.tw/index.php/faculty/more/286" target="_blank">陳春樹</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncu.edu.tw/index.php/faculty/more/286" target="_blank">陳春樹</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.math.ccu.edu.tw/math/?page_id=237" target="_blank">林培生</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.tku.edu.tw/info/56" target="_blank">張雅梅</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncu.edu.tw/index.php/faculty/more/311" target="_blank">邱詠惠</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"></td>
+            </tr>
+            <tr>
+              <td valign="top" bgcolor="#FFFFFF">6</td>
+              <td align="center" bgcolor="#FFFFFF">Industrial Statistics and Its Application</td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncku.edu.tw/index.php?option=module&lang=cht&task=pageinfo&id=8&index=1" target="_blank">陳瑞彬</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncku.edu.tw/index.php?option=module&lang=cht&task=pageinfo&id=8&index=1" target="_blank">陳瑞彬</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.ncu.edu.tw/index.php/faculty/more/257" target="_blank">張明中</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="https://w2.math.ncu.edu.tw/member/full/64" target="_blank">黃世豪</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"><p><a href="http://www.stat.nuk.edu.tw/hsuhl/" target="_blank">許湘伶</a></p></td>
+              <td align="center" bgcolor="#FFFFFF"></td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
+    </div>
+
+<!-- 這裡結束 -->
+          </div>
+
+      </div>
+    </section>
+
+<?php include_once "footer.php" ?>
+</body>
+</html>
